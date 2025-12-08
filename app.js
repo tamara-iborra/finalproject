35abb719a26a77b6b8a6c094c65a72f4dc02a7ac
Tamara Iborra <taimon1997@hotmail.com>
2025-12-08 19:18:41 +0100

    create middleware to block private routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import { engine } from "express-handlebars";
+import cookieParser from 'cookie-parser';
+
 import cloudinary from "./config/cloudinary.js";
 import { db, initLowDB } from "./config/lowdb.js";
 import upload from "./config/multer.js";
+// Middlewares
+import authenticateToken from './middleware/auth.js';
 // Rutas
 import usersRoutes from "./routes/users.routes.js";
 import { authWebRouter } from "./routes/auth/auth.web.js";
@@ -23,6 +27,7 @@ app.set("views", "./views");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Montar rutas externas
 app.use("/auth", authWebRouter);
@@ -33,7 +38,7 @@ app.get("/", async (_req, res) => {
   res.render("landing");
 });
 
-app.get("/home", (_req, res) => {
+app.get("/home", authenticateToken, (_req, res) => {
   res.render("home", { title: "Home" });
 });
 
